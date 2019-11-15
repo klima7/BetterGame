3e18781630e16f07430a73a59c89bcb800669dd8
Lukasz Klimkiewicz <ukasz.klimkiewicz2@wp.pl>
2019-11-15 11:24:51 +0100

    Better client and client_human template

--- client_common.h
+++ client_common.h
@@ -1,5 +1,10 @@
 #ifndef __CLIENT_COMMON_H__
 #define __CLIENT_COMMON_H__
 
+#include "common.h"
+
+typedef enum action_t(*behaviour_fun_t)(void);
+
+void clientc_run(enum client_type_t client_type);
 
 #endif
